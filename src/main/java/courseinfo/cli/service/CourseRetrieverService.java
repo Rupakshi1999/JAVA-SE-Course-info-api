74090f65b2247294289d502ff378b7e703625f64
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2024-01-05 12:01:42 -0500

    handle response error codes

--- src/main/java/courseinfo/cli/service/CourseRetrieverService.java
+++ src/main/java/courseinfo/cli/service/CourseRetrieverService.java
@@ -8,7 +8,10 @@ import java.net.http.HttpResponse;
 
 public class CourseRetrieverService {
     private static final String PS_URI = "https://app.pluralsight.com/profile/data/author/%s/all-content/";
-    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final HttpClient CLIENT = HttpClient
+            .newBuilder()
+            .followRedirects(HttpClient.Redirect.ALWAYS)
+            .build();
     public String getCourses(String authorID) {
         HttpRequest request = HttpRequest
                 .newBuilder(URI.create(PS_URI.formatted(authorID)))
@@ -17,7 +20,12 @@ public class CourseRetrieverService {
 
         try {
             HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            // return response, deal with error codes
+            return switch(response.statusCode()) {
+                case 200 -> response.body();
+                case 404 -> "Not found";
+                default -> throw new RuntimeException("Pluralsight API failed with status code: " + response.statusCode());
+            };
         } catch (IOException | InterruptedException e){
             throw new RuntimeException("Could not call Pluralsight API", e);
         }
