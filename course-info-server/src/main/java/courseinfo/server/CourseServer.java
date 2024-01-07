30fc80fa7377ac62ad3171d7fc793a6349aa19ba
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2024-01-06 19:42:39 -0500

    add a server using jersey server to listn on localhost port 8080

---
+++ course-info-server/src/main/java/courseinfo/server/CourseServer.java
@@ -0,0 +1,25 @@
+package courseinfo.server;
+
+import courseinfo.repository.CourseRepository;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+public class CourseServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
+    private static final String BASE_URI = "http://localhost:8080/";
+
+    public static void main(String... args){
+        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
+
+
+        // Create HTTP server
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+        LOG.info("Server is listening on " + BASE_URI);
+    }
+}
